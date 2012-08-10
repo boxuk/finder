7428ef8544b40540e677b3d226db34d80b527ca1
rod <rod@pu-gh.com>
2012-08-10 07:32:23 +0100

    added lein-midje dev dependency

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
 (defproject finder "0.0.1"
   :description "Simple library for create clojure.java.jdbc queries"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [midje "1.4.0"]])
+                 [midje "1.4.0"]]
+  :dev-dependencies [[lein-midje "1.0.9"]])
 
