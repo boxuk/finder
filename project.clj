53fe5f0cf99c14b39d3a84c1f019debb3d205b52
rhodri <rhodri.pugh@boxuk.com>
2012-08-10 08:44:36 +0100

    added clojars dep and renamed project (finder already taken)

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 
-(defproject finder "0.0.1"
+(defproject sql-finder "0.0.1"
   :description "Simple library for create clojure.java.jdbc queries"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [midje "1.4.0"]]
-  :dev-dependencies [[lein-midje "1.0.9"]])
+  :dev-dependencies [[lein-midje "1.0.9"]
+                     [lein-clojars "0.9.0"]])
 
