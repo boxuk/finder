ac5665e8871f0b5f49bb506f16816445e164257d
rhodri <rhodri.pugh@boxuk.com>
2012-08-14 16:07:08 +0100

    bumped version to 1.0.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject sql-finder "1.0.0"
+(defproject sql-finder "1.0.1"
   :description "Simple library for create clojure.java.jdbc queries"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [midje "1.4.0"]]
